ffc73e9a16cbbfb84259f1f52f796173d8179355
ylchin <chin.yiling0715@gmail.com>
2025-10-14 17:01:35 +0800

    logger

---
+++ backend/services/question_service/src/utils/logger.ts
@@ -0,0 +1,7 @@
+// src/utils/logger
+
+export const log = {
+    info: (...a: any[]) => console.log("[INFO]", ...a),
+    warn: (...a: any[]) => console.warn("[WARN]", ...a),
+    error: (...a: any[]) => console.error("ERROR", ...a)
+};
