72b28af6625cdd1513841bca5a42cb0b8ba3fa25
ylchin <chin.yiling0715@gmail.com>
2025-10-14 23:55:55 +0800

    initialise prisma client

---
+++ backend/services/question_service/src/repositories/prisma.ts
@@ -0,0 +1,9 @@
+// src/repositories/prisma
+
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
