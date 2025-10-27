591f9ca3dde44f5c6d10dfc2b90c2001980e4bbc
danielleloh <szehan.loh@gmail.com>
2025-10-27 22:16:56 +0800

    feat(matching service): add tests for matching worker and data structures

---
+++ backend/services/matching_service/src/test/test_data.ts
@@ -0,0 +1,15 @@
+import { Difficulty } from '../clients/redis/types.js';
+
+export type UserInput = {
+  userId: string;
+  difficulty: Difficulty;
+  topics: string[];
+};
+
+export const Users: UserInput[] = [
+  { userId: '001', difficulty: 'Easy', topics: ['Array', 'Strings'] }, // no match -> match with 5
+  { userId: '002', difficulty: 'Medium', topics: ['Array'] }, // no match -> clear
+  { userId: '003', difficulty: 'Easy', topics: ['DP'] }, // no match -> potential macth with 5
+  { userId: '004', difficulty: 'Hard', topics: ['Array'] }, // no match -> not eligible for match
+  { userId: '005', difficulty: 'Easy', topics: ['Array', 'DP'] }, // match with 1
+];
