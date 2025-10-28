446d1d7357e37c0a471e98b31c4a0aa51fa10fb5
danielleloh <szehan.loh@gmail.com>
2025-10-28 14:20:17 +0800

    feat(matching service): implement main entry point for matching service.

--- backend/services/matching_service/src/main.ts
+++ backend/services/matching_service/src/main.ts
@@ -1,106 +1,41 @@
-import { MatchingServiceRedis } from './redis/redisClient.js';
-import { UserData } from './redis/types.js';
+import { MatchingServiceRedis } from './clients/redis/redis_client.js';
+import { MatchingWorker } from './workers/matching_worker.js';
+import { test_disconnect_sweeper } from './test/test_disconnect_sweeper.js';
+import { TTLHandler } from './workers/ttl_handler.js';
+import { TTLSubscriber } from './clients/redis/ttl_subscriber.js';
+import { DisconnectSweeper } from './workers/disconnect_sweeper.js';
+import { logger } from './logger/logger.js';
+// import { test_matching_worker } from './test/test_matching_worker.js';
+// import { test_data_structures } from './test/test_data_structures.js';
+// import { test_ttl_subscriber } from './test/test_ttl_subscriber.js';
 
+/**
+ * Entry point to matching service.
+ * Instantiates and starts processes needed to run the matching service.
+ */
 async function main() {
   try {
-    const redis = await MatchingServiceRedis.getInstance();
+    const matchingRedis = await MatchingServiceRedis.getInstance();
+    const worker = await MatchingWorker.getInstance();
 
-    // test
-    const id = 'user1';
-    const data: UserData = {
-      status: 'waiting',
-      difficulty: 'Easy',
-      topics: ['Algorithms', 'DP'],
-      lastSeen: Date.now(),
-    };
-    await redis.statusHash.addUser(id, data);
-    const returnData = await redis.statusHash.getUserData('user1');
-    console.log('Test HSET:', returnData);
+    const ttlHandler = new TTLHandler(matchingRedis);
+    const subscriber = new TTLSubscriber(ttlHandler);
 
-    await redis.entryQueue.enqueue(id);
-    const deqData = await redis.entryQueue.dequeue();
-    console.log('Test Q:', deqData);
+    const sweeper = new DisconnectSweeper(matchingRedis, false);
 
-    console.log('TEST: MatchingPool ============');
-    const difficulty = 'easy';
-    const topic = 'arrays';
+    // start all three processes
+    await Promise.all([
+      worker.start(),
+      subscriber.subscribe(),
+      sweeper.start(),
+    ]);
 
-    console.log('➡️ Enqueueing users...');
-    await redis.matchingPool.enqueueUser('user1', difficulty, topic);
-    await redis.matchingPool.enqueueUser('user2', difficulty, topic);
-    await redis.matchingPool.enqueueUser('user3', difficulty, topic);
-    console.log('✅ Enqueued user1, user2, user3.');
-
-    const peeked = await redis.matchingPool.peekQueue(difficulty, topic);
-    console.log(`👀 Peeked user: ${peeked}`);
-
-    console.log('⬇️ Dequeuing users...');
-    const dequeued1 = await redis.matchingPool.dequeueUser(difficulty, topic);
-    const dequeued2 = await redis.matchingPool.dequeueUser(difficulty, topic);
-    const dequeued3 = await redis.matchingPool.dequeueUser(difficulty, topic);
-    const dequeued4 = await redis.matchingPool.dequeueUser(difficulty, topic);
-    console.log(
-      `✅ Dequeued users: ${dequeued1}, ${dequeued2}, ${dequeued3}, ${dequeued4}`,
-    );
-
-    console.log('⬇️ Test dequeuing users from non-existent pool...');
-    const deqNoExist = await redis.matchingPool.dequeueUser('Medium', 'DP');
-    console.log(`✅ Dequeued users: ${deqNoExist}`);
-
-    console.log('🧹 Clearing all queues...');
-    await redis.matchingPool.clearAllQueues();
-
-    console.log('✅ MatchingPool test completed.\n');
-
-    console.log('==================================');
-    console.log('FCFS list test:');
-    console.log('==================================');
-
-    await redis.fcfsList.enqueueUser('user1');
-    await redis.fcfsList.enqueueUser('user2');
-    await redis.fcfsList.enqueueUser('user3');
-
-    console.log('--- Check positions ---');
-    console.log(
-      'Position of user1:',
-      await redis.fcfsList.getUserPosition('user1'),
-    ); // 0
-    console.log(
-      'Position of user2:',
-      await redis.fcfsList.getUserPosition('user2'),
-    ); // 1
-    console.log(
-      'Position of user3:',
-      await redis.fcfsList.getUserPosition('user3'),
-    ); // 1
-    console.log(
-      'Position of user4 (not in queue):',
-      await redis.fcfsList.getUserPosition('user4'),
-    ); // null
-
-    console.log('--- Remove user2 ---');
-    await redis.fcfsList.removeUser('user2');
-    console.log(
-      'Position of user1 after removal:',
-      await redis.fcfsList.getUserPosition('user1'),
-    ); // 0
-    console.log(
-      'Position of user2 after removal:',
-      await redis.fcfsList.getUserPosition('user2'),
-    ); // 0
-    console.log(
-      'Position of user3 after removal:',
-      await redis.fcfsList.getUserPosition('user3'),
-    ); // 0
-
-    console.log('--- Clear list ---');
-    await redis.fcfsList.clearList();
-    console.log(
-      'Position of user1 after clearing:',
-      await redis.fcfsList.getUserPosition('user1'),
-    ); // null
+    // await test_data_structures();
+    // await test_matching_worker();
+    // await test_ttl_subscriber();
+    await test_disconnect_sweeper();
   } catch (err) {
-    console.error('Error: ', err);
+    logger.error('[main] Error: ', err);
     process.exit(1);
   }
 }
