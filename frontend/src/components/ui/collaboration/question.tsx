779ba667d33f24a8bdfbcfdba5e89d8a33b6eac9
Kailash201 <109354852+Kailash201@users.noreply.github.com>
2025-10-30 18:44:25 +0800

    Add questionAPI integration into fe

--- frontend/src/components/ui/collaboration/question.tsx
+++ frontend/src/components/ui/collaboration/question.tsx
@@ -7,57 +7,63 @@ import {
   Typography,
 } from '@mui/material';
 import '@/styles/globals.css';
+import ReactMarkdown from 'react-markdown';
+import { getQuestionById } from '@/services/questionServiceApi';
+import { useEffect, useState } from 'react';
+import { getQuestionIdBySessId } from '@/services/collaborationServiceApi';
+import { Question, Topic } from '@/lib/question-service';
 
-export const QuestionCard = () => {
-  const question_string = `Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem
+interface questionProps {
+  sessionId: string;
+}
 
-To add when question service is up
+export const QuestionCard = (p: questionProps) => {
+  const { sessionId } = p;
 
+  const [questionTitle, setQuestionTitle] = useState('');
+  const [questionMd, setQuestionMd] = useState('');
+  const [questionTopics, setQuestionTopics] = useState<Topic[]>([]);
 
-Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem
-Lorem Lorem`;
+  useEffect(() => {
+    const fetchQuestion = async () => {
+      const questionId: string | null = await getQuestionIdBySessId(sessionId);
+      console.log(1, questionId);
+      if (!questionId) return;
+      const question: Question | null = await getQuestionById(questionId);
+      console.log(question);
+      setQuestionMd(question?.body_md ?? '');
+      setQuestionTitle(question?.title ?? '');
+      setQuestionTopics(question?.topics ?? []);
+    };
+    fetchQuestion();
+  }, []);
 
   return (
     <Card className="rounded-2xl shadow-xl h-full w-full" variant="outlined">
       <CardHeader
         title={
           <Typography variant="h5" fontWeight="bold">
-            Two Sum
+            {questionTitle}
           </Typography>
         }
         subheader={
           <Stack direction="row" spacing={1}>
-            <Chip
-              label="Array"
-              variant="outlined"
-              size="medium"
-              className="border border-blue-500 text-blue-500 min-w-10"
-            />
-            <Chip
-              label="Array"
-              variant="outlined"
-              size="medium"
-              className="border border-blue-500 text-blue-500 min-w-10"
-            />
-            <Chip
-              label="Array"
-              variant="outlined"
-              size="medium"
-              className="border border-blue-500 text-blue-500 min-w-10"
-            />
-            <Chip
-              label="Array"
-              variant="outlined"
-              size="medium"
-              className="border border-blue-500 text-blue-500 min-w-10"
-            />
+            {questionTopics.map((topic: Topic, idx: number) => (
+              <Chip
+                key={idx}
+                label={topic.slug}
+                variant="outlined"
+                size="medium"
+                className="border border-blue-500 text-blue-500 min-w-10"
+              />
+            ))}
           </Stack>
         }
       />
       <CardContent>
         <Stack>
           <Typography variant="body1" sx={{ whiteSpace: 'pre-wrap' }}>
-            {question_string}
+            <ReactMarkdown>{questionMd}</ReactMarkdown>
           </Typography>
         </Stack>
       </CardContent>
