6c4793de4bd63e724c8dc716e7454d4858245d66
danielleloh <szehan.loh@gmail.com>
2025-11-05 16:00:51 +0800

    fix(matching service): fix build error

--- frontend/src/components/ui/matching/TimeoutView.tsx
+++ frontend/src/components/ui/matching/TimeoutView.tsx
@@ -6,7 +6,7 @@ export default function TimeoutView() {
   return (
     <div className="flex flex-col items-center text-center space-y-5 w-full">
       <h1 className="text-violet-500">
-        We couldn't find you a peer right now :(
+        We could not find you a peer right now :(
       </h1>
       <p className="text-gray-500">
         Try again in a few minutes, or adjust your difficulty/topic filters and
